10b40cb0fef3d849a7143f00d3904dd642efc5da
Rob <Rob@LAPTOP-D04HPS34>
2019-02-04 21:25:33 +0000

    Changed titles and headers, changed content to currencies

---
+++ view/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+    <title>Currency Exchange</title>
+    <link href="../main.css" rel="stylesheet" type="text/css"/>
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1>Currency Exchange</h1></header>
+       
